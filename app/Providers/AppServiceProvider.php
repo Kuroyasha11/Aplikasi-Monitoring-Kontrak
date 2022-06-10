e153065ce49e5f2d3712136e365dd1f76a52dbd6
Kuroyasha <Rickyandrean41@gmail.com>
2022-06-10 19:20:49 +0700

    menambah view login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+
+        Blade::directive('IDR', function ($rupiah) {
+            return "Rp<?php echo number_format($rupiah, 0); ?>";
+        });
     }
 }
