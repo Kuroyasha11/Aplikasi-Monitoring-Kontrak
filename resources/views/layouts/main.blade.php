76667c62de21864ca461f5d073bc72cfa3eff5c0
byy7 <muhammad.faraby710@gmail.com>
2022-06-15 22:07:49 +0700

    ganti nama title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title }} | PT BGR LOGISTIK INDONESIA DIVRE PALEMBANG</title>
+    <title>{{ $title }} | PT BGR LOGISTIK INDONESIA DIVRE PALEMBANGS</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
