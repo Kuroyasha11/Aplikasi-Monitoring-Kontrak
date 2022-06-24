12296b44348cfcf9be41ad02f4b14e20ac83a894
Kuroyasha <Rickyandrean41@gmail.com>
2022-06-24 11:47:42 +0700

    memperbaikki tampilan data tables (auth)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,37 +18,41 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <table id="tabel-print" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Rendering engine</th>
-                            <th>Browser</th>
-                            <th>Platform(s)</th>
-                            <th>Engine version</th>
-                            <th>CSS grade</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 4.0
-                            </td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>X</td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>Rendering engine</th>
-                            <th>Browser</th>
-                            <th>Platform(s)</th>
-                            <th>Engine version</th>
-                            <th>CSS grade</th>
-                        </tr>
-                    </tfoot>
-                </table>
+                @auth
+                    <table id="tabel-print" class="table table-bordered table-striped">
+                    @else
+                        <table id="tabel-biasa" class="table table-bordered table-striped">
+                        @endauth
+                        <thead>
+                            <tr>
+                                <th>Rendering engine</th>
+                                <th>Browser</th>
+                                <th>Platform(s)</th>
+                                <th>Engine version</th>
+                                <th>CSS grade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 4.0
+                                </td>
+                                <td>Win 95+</td>
+                                <td> 4</td>
+                                <td>X</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Rendering engine</th>
+                                <th>Browser</th>
+                                <th>Platform(s)</th>
+                                <th>Engine version</th>
+                                <th>CSS grade</th>
+                            </tr>
+                        </tfoot>
+                    </table>
             </div>
             <!-- /.card-body -->
         </div>
